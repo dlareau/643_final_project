b31216d59211f4a62bbf3b2e5cb0f165aa33f26e
Dillon Lareau <flybye22@gmail.com>
2016-11-20 12:47:14 -0500

    Sudoku now working, can solve non-guessing puzzles (4x4 and 9x9 tested)

--- sudoku.sv
+++ sudoku.sv
@@ -2,6 +2,7 @@ module top #(parameter WIDTH = 9, N = 3) (
     input logic [WIDTH-1:0][WIDTH-1:0][WIDTH-1:0] initial_vals,
     input logic start, clock, reset_L,
     output logic [WIDTH-1:0][WIDTH-1:0][WIDTH-1:0] final_vals,
+    output logic [WIDTH-1:0][WIDTH-1:0][3:0] human_readable_vals,
     output logic fail);
 
     logic [WIDTH-1:0][WIDTH-1:0][WIDTH-1:0] options, new_options;
@@ -13,6 +14,7 @@ module top #(parameter WIDTH = 9, N = 3) (
         genvar row, col;
         for(row = 0; row < WIDTH; row++) begin: row_square
             for(col = 0; col < WIDTH; col++) begin: col_square
+                bcd_encoder bcde(final_vals[row][col], human_readable_vals[row][col]);
                 square #(WIDTH, N) s(.row(row_vals[row]), .col(col_vals[col]), .sector(sector_vals[(row/N)*N + (col/N)]),
                        .det_val(new_options[row][col]),
                        .initial_val(initial_vals[row][col]), .clock, .reset_L,
@@ -114,20 +116,21 @@ module square #(parameter WIDTH = 9, N = 3) (
     output [WIDTH-1:0] options_out, final_val,
     output logic fail);
 
-    logic [WIDTH-1:0] internal_det_val, load_val, options, options_before_latch;
-    logic valid;
+    logic [WIDTH-1:0] internal_det_val, load_val, options;//, options_before_latch;
+    logic valid, load;
 
     assign internal_det_val = (det_val == 0) ? det_val : ~det_val;
     assign options = row | col | sector | internal_det_val;
     one_hot_detector #(WIDTH) ohd(~options, valid);
     assign load_val = load_initial ? initial_val : ~options;
+    assign load = (valid || load_initial) && !(final_val);
 
-    register #(WIDTH) r1(final_val, load_val, (~valid || load_initial), clock, reset_L);
-    register #(WIDTH) r2(options_out, options_before_latch, (~valid || load_initial), clock, reset_L);
+    register #(WIDTH) r1(final_val, load_val, ~(load), clock, reset_L);
+    //register #(WIDTH) r2(options_out, options_before_latch, ~(valid || load_initial), clock, reset_L);
 
     assign all_ones = &options;
     assign fail = all_ones && (final_val == 0);
-    assign options_before_latch = (final_val == 0) ? options : {WIDTH-1{1'b1}};
+    assign options_out = (final_val == 0) ? (row | col | sector) : {WIDTH{1'b1}};
 
 endmodule: square
 
@@ -156,12 +159,35 @@ module register #(parameter WIDTH = 16) (
 
 endmodule
 
+module bcd_encoder(
+    input logic [8:0] in,
+    output logic [3:0] out);
+
+    always_comb begin
+        case(in)
+            9'b000000000: out = 0;
+            9'b000000001: out = 1;
+            9'b000000010: out = 2;
+            9'b000000100: out = 3;
+            9'b000001000: out = 4;
+            9'b000010000: out = 5;
+            9'b000100000: out = 6;
+            9'b001000000: out = 7;
+            9'b010000000: out = 8;
+            9'b100000000: out = 9;
+            default: out = 0;
+        endcase
+    end
+
+endmodule: bcd_encoder
+
 module solver_TB;
 
-  logic [3:0][3:0][3:0] initial_vals, final_vals;
+  logic [8:0][8:0][8:0] initial_vals, final_vals;
+  logic [8:0][8:0][3:0] human_readable_vals;
   logic start, clock, reset_L, fail;
 
-  top #(4, 2) DUT(.*);
+  top #(9, 3) DUT(.*);
 
   initial begin
       clock = 0;
@@ -169,8 +195,20 @@ module solver_TB;
   end
 
   initial begin
+    $monitor("Values @ %d\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h", $time,
+             human_readable_vals[0], human_readable_vals[1], human_readable_vals[2],
+             human_readable_vals[3], human_readable_vals[4], human_readable_vals[5],
+             human_readable_vals[6], human_readable_vals[7], human_readable_vals[8]);
     reset_L = 1;
-    initial_vals = 64'h0040_2000_1200_0001;
+initial_vals[0] = 81'b000000000_010000000_000000000_000000000_000000000_000000000_000000001_000010000_000000000_;
+initial_vals[1] = 81'b000001000_000000000_000100000_000010000_000000000_100000000_000000000_010000000_000000000_;
+initial_vals[2] = 81'b000000000_000000000_000000000_000000000_000000000_010000000_000000000_000000000_000000000_;
+initial_vals[3] = 81'b000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_;
+initial_vals[4] = 81'b000000000_000000000_000000010_000000000_000001000_000000000_000000000_000000000_000000100_;
+initial_vals[5] = 81'b000000100_000000000_000000000_010000000_000000000_000000001_000000000_000000000_000000000_;
+initial_vals[6] = 81'b100000000_000000000_000000000_000000000_001000000_000000000_000000000_000000000_000000000_;
+initial_vals[7] = 81'b000100000_000000000_000000000_000000000_000000000_000000000_000000000_000000000_000001000_;
+initial_vals[8] = 81'b000000001_000010000_000000000_000000000_000000000_000000000_000000000_100000000_000000000_;
     #2;
     reset_L <= 0;
     @(posedge clock);
